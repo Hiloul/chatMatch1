e888ec65392fe4b7dd236eb4fa1a9389fce71ade
Hilel Hamidi <hilel.hamidi@gmail.com>
2022-10-14 09:52:01 +0200

    placeholder

--- src/views/CreateAccountView.vue
+++ src/views/CreateAccountView.vue
@@ -1,10 +1,28 @@
 <template>
     <h1>Créer un compte</h1>
     <form action="">
-        <input type="text">
-        <input type="text">
-        <input type="text">
-        <input type="text">
-        <input type="text">
+        <input type="text" placeholder="Nom">
+        <input type="text" placeholder="Prénom">
+        <input type="email" placeholder="Mail">
+        <input type="text" placeholder="Pseudo">
+        <input type="text" placeholder="Mot de passe">
     </form>
 </template>
+
+<style scoped>
+h1{text-align: center;
+text-decoration: underline;
+padding: 5px;
+color: #FE8C0E;}
+form{
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    padding: 20px;
+}
+input{
+    padding: 10px;
+    margin: 10px;
+    border-radius: 10px;
+}
+</style>
